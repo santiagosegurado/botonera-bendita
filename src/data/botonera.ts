b556a64689cbadab81f967fce99cc846b49c4d15
Santiago <santisegurado1@gmail.com>
2025-03-08 04:19:07 -0300

    Agregar archivo de configuración de la botonera con rutas de audio

---
+++ src/data/botonera.ts
@@ -0,0 +1,37 @@
+export const botonera = [
+    {
+        id: 1,
+        name: 'Impacto',
+        path: '/audio/impacto.mp3',
+    },
+    {
+        id: 2,
+        name: 'Baaassta',
+        path: '/audio/basta-jacobo.mp3',
+    },
+    {
+        id: 3,
+        name: 'Dale Ponelo',
+        path: '/audio/dale-ponelo-jacobo.mp3',
+    },
+    {
+        id: 4,
+        name: 'Creo que se equivoco',
+        path:'/audio/creo-que-se-equivoco.mp3',
+    },
+    {
+        id: 5,
+        name: 'Sientate Gorda',
+        path:'/audio/sientate-gorda.mp3',
+    },
+    {
+        id: 6,
+        name: 'Expedientes X 🎶',
+        path:'/audio/expedientes-x.mp3',
+    },
+    {
+        id: 7,
+        name: 'Lluvia de chanes',
+        path:'/audio/lluvia-de-chanes.mp3',
+    },
+];
